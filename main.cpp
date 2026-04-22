97f88e4c38816b8d5b7defbf858bd0d2f5cfd32b
Thomas Morrison <tmorrison6794@sdsu.edu>
2026-04-22 11:35:33 -0700

    Commit 3

--- main.cpp
+++ main.cpp
@@ -117,9 +117,17 @@ void printPath(pair<int,int> exitcell,
 // STUDENTS IMPLEMENT DFS HERE
 // Add arguments, return type, and logic
 // ----------------------------------------------------------
-// bool dfs(……) {
-//     // Your code here
-// }
+ bool dfs(int r,int c,
+     const vector<vector<int>>& maze,
+     vector<vector<bool>>& visited,
+     vector<vector<int>>& parent_r,
+     vector<vector<int>>& parent_c,
+     int ent_r, int ent_c,)
+{
+    int N = maze.size();
+    int M = maze[0].size();
+
+}
 
 
 // ----------------------------------------------------------
